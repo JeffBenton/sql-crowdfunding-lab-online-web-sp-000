e7037f87db3a9329872f7c94468673ea505282a3
Jeff Benton <sikletz@gmail.com>
2019-04-12 18:20:34 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE projects
-  (id PRIMARY KEY INTEGER,
+  (id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
   funding_goal TEXT,
@@ -8,13 +8,13 @@ CREATE TABLE projects
   )
   
 CREATE TABLE user
-  (id PRIMARY KEY INTEGER,
+  (id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
   )
 
 CREATE TABLE pedge
-  (id PRIMARY KEY INTEGER,
+  (id INTEGER PRIMARY KEY,
   amount REAL,
   user_id INTEGER,
   project_id INTEGER
